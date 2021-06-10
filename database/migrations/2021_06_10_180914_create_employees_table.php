aa12f2d8d32d95fe264f6fb9d3886db0f48c452f
unknown <mathiarasu22@gmail.com>
2021-06-11 00:01:09 +0530

    AFter migrations

---
+++ database/migrations/2021_06_10_180914_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id('emp_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('comp_id');
+            $table->string('email');
+            $table->integer('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}

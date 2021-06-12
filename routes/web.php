5df21474c7dff00b6f40442963a4fc95dfe26fb6
unknown <mathiarasu22@gmail.com>
2021-06-12 19:43:23 +0530

    commit before github push

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-
-
 Route::group(['middleware' => ['auth']], function () {
             
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
